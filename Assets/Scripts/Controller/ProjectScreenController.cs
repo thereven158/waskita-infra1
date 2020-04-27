895050aadda25477f423102c07db1d9e512b5a9b
Muhammad Yoga Nugraha Putra <m.yoga@agate.id>
2020-04-27 16:12:05 +0700

    Project Screen UI

---
+++ Assets/Scripts/Controller/ProjectScreenController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectScreenController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform SpawnPoint;
+
+    [SerializeField]
+    private GameObject content;
+
+    [SerializeField]
+    private RectTransform contentRect;
+
+    [SerializeField]
+    private int numberOfItems = 5;
+
+    public string[] itemNames;
+
+    ProjectScreenView _projectScreenView = new ProjectScreenView();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _projectScreenView.CreateContent(SpawnPoint, content, contentRect, numberOfItems, itemNames);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectProject()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Controller/ProjectScreenView.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectScreenView : MonoBehaviour
+{
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateContent(Transform SpawnPoint, GameObject content, RectTransform contentRect, int numberOfItems, string[] itemNames)
+    {
+        //setContent Holder Height;
+        //contentRect.sizeDelta = new Vector2(0, numberOfItems * 60);
+        Debug.Log(SpawnPoint.localPosition.y);
+
+        for (int i = 0; i < numberOfItems; i++)
+        {
+            // 60 width of item
+            float spawnY = i * 70;
+
+            //newSpawn Position
+            Vector3 pos = new Vector3(SpawnPoint.localPosition.x, SpawnPoint.localPosition.y + (-spawnY), SpawnPoint.position.z);
+
+            //instantiate item
+            GameObject SpawnedItem = Instantiate(content, pos, SpawnPoint.rotation);
+
+            //setParent
+            SpawnedItem.transform.SetParent(contentRect, false);
+
+            //get ItemDetails Component
+            ProjectDetail itemDetails = SpawnedItem.GetComponent<ProjectDetail>();
+
+            //set name
+            itemDetails.text.text = itemNames[i];
+
+        }
+    }
+}
