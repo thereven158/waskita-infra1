f59acc458f92a9989e1b246361c0e2e12f9dd1e6
A. Billy Firman Prasetya S <billy.firman@agate.id>
2020-05-01 11:53:07 +0700

    Level List Display Layout And Logic
    creating new since the made one are coupled with brief display

---
+++ Assets/Scripts/UserInterface/LevelList/LevelDataDisplay.cs
@@ -0,0 +1,33 @@
+using A3.Unity;
+using Agate.WaskitaInfra1.Level;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Agate.WaskitaInfra1.UserInterface.LevelList
+{
+    public class LevelDataDisplay: InteractiveDisplayBehavior<LevelData>
+    {
+        [SerializeField]
+        private Button _button = default;
+        [SerializeField]
+        private TMP_Text _nameText = default;
+
+        internal Button Button => _button;
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(Interaction);
+        }
+
+        protected override void ConfigureDisplay(LevelData data)
+        {
+            _nameText.text = data.Name;
+        }
+
+        private void Interaction()
+        {
+            Interact();
+        }
+    }
+}
