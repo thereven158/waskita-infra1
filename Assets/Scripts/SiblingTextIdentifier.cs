8eaa2e6577aea00f5b51fbacd9e93e17b7f6094a
A. Billy Firman Prasetya S <billy.firman@agate.id>
2020-05-14 13:50:35 +0700

    small tweak
    remove old prefab to avoid misinformation

--- Assets/Scripts/SiblingTextIdentifier.cs
+++ Assets/Scripts/SiblingTextIdentifier.cs
@@ -6,7 +6,7 @@ public class SiblingTextIdentifier : MonoBehaviour
     [SerializeField]
     private TMP_Text _text;
 
-    private void Awake()
+    private void OnEnable()
     {
         _text.text = "" + (char)( 0x41 + transform.GetSiblingIndex());
     }
